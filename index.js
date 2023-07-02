0d0cd0296271867c428812ac4b5bbf7908551366
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-07-02 15:29:49 +0430

    example of async javascript code

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 console.log("before");
 // when we are calling an async function it doesn't execute right-away it just schedules a task to be perform in the future
 // and when the interpreter is free and there is no synchronous task to perform that task is executed
-setTimeout(() => {
-    console.log("logging a message asynchronously");
-}, 2000);
+getUser(400, function(user){
+    console.log(user);
+    gitRepositories(user, (repos) => {
+        console.log(repos);
+    })
+});
 
 console.log("after");
+
+// in order to access data from an asynchronous operation we need to use one of the following methods
+// - callbacks
+// - promises
+// - async/await
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Getting a user from database...");
+    // we have no access to this return value because it is run 2s latter 
+    callback({ name: "mostafa", id: id, githubAccount: "mostafa-meerzad" });
+  }, 2000);
+}
+
+function gitRepositories(user, callback){
+    setTimeout(() => {
+        console.log(`${user.name}'s repos`)
+        callback(["repo1", "repo2", "repo3", "repo4"])
+    }, 2000);
+    
+}
