2afc898947b1a296de1f011252d9cd16062b818b
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-07-02 15:51:33 +0430

    a solution for callback hell

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 console.log("before");
 // when we are calling an async function it doesn't execute right-away it just schedules a task to be perform in the future
 // and when the interpreter is free and there is no synchronous task to perform that task is executed
-getUser(400, function(user){
-    console.log(user);
-    gitRepositories(user, (repos) => {
-        console.log(repos);
-    })
-});
+getUser(400, displayUser);
 
+function displayRepos(repos) {
+  console.log(repos)
+}
+
+function displayUser(user) {
+  console.log(user);
+  gitRepositories(user, displayRepos)
+}
 console.log("after");
 
 // in order to access data from an asynchronous operation we need to use one of the following methods
